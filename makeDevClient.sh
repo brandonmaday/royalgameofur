329b46866210b304f9c507ab42965ee738d64c23
Brandon Maday <brandon.maday@gmail.com>
2020-06-15 18:03:19 -0400

    elm client release version

--- makeDevClient.sh
+++ makeDevClient.sh
@@ -3,5 +3,5 @@
 cd client
 if elm make src/Main.elm --output=elm.bin.js;
 then
-    mv elm.bin.js ../static
+    mv elm.bin.js ../static/elm-0.0.1.bin.js
 fi
--- makeProdClient.sh
+++ makeProdClient.sh
@@ -1,7 +1,10 @@
 #! /bin/zsh
 
 cd client
-if elm make src/Main.elm --output=elm.bin.js;
-then
-    mv elm.bin.js ../static
-fi
+
+elm make src/Main.elm --optimize --output=elm.js
+
+uglifyjs elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=elm.min.js
+
+mv elm.min.js ../static/elm-0.0.1.bin.js
+rm elm.js
